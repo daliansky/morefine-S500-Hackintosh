93719d0ade80f32eee96b9e3fb8aabe485fc7824
DalianSky <daliansky2008@gmail.com>
2022-06-27 13:27:23 +0800

    更新morefine-s500.sh

--- Tools/morefine-s500.sh
+++ Tools/morefine-s500.sh
@@ -5,7 +5,10 @@
 # @email [daliansky2008@mail.com]
 # @create date 2022-05-06 09:23:13
 # @modify date 2022-05-06 09:23:13
-#
+# 调用url: sh -c "$(curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/daliansky/morefine-S500-Hackintosh/main/Tools/morefine-s500.sh)"
+echo "正在执行S500一键优化脚本，该脚本包括："
+echo "启用macOS安装应用允许任何来源 / 修复睡眠唤醒 / 修复 无法登录App Store；"
+echo "请输入当前用户的密码，然后回车"
 sudo spctl --master-disable		# 启用macOS安装应用允许任何来源
 sudo rm -rf /Library/Preferences/SystemConfiguration/NetworkInterfaces.plist*
 sudo pmset -b hibernatemode 0		# 内存供电，内存镜像不写入硬盘
